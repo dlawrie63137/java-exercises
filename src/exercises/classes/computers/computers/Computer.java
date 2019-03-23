bbbe7eb06b48b3ce3b8744ac915d8a09c960dacd
David Lawrie <dlawrie@att.net>
2019-03-23 14:08:54 -0500

    Class 7 exercises

---
+++ src/exercises/classes/computers/computers/Computer.java
@@ -0,0 +1,59 @@
+package exercises.classes.computers.computers;
+
+public class Computer {
+
+    private String name;
+    private String manufacturer;
+    private String ramType;
+    private int ramSize;
+    private String cpuType;
+    private int hddSize;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getRamType() {
+        return ramType;
+    }
+
+    public void setRamType(String ramType) {
+        this.ramType = ramType;
+    }
+
+    public int getRamSize() {
+        return ramSize;
+    }
+
+    public void setRamSize(int ramSize) {
+        this.ramSize = ramSize;
+    }
+
+    public String getCpuType() {
+        return cpuType;
+    }
+
+    public void setCpuType(String cpuType) {
+        this.cpuType = cpuType;
+    }
+
+    public int getHddSize() {
+        return hddSize;
+    }
+
+    public void setHddSize(int hddSize) {
+        this.hddSize = hddSize;
+    }
+}
---
+++ src/exercises/classes/computers/computers/Laptop.java
@@ -0,0 +1,88 @@
+package exercises.classes.computers.computers;
+
+public class Laptop extends Computer {
+
+    private String name;
+    private String manufacturer;
+    private String ramType;
+    private int ramSize;
+    private String cpuType;
+    private int hddSize;
+    private int screenSize;
+    private double weight;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    @Override
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    @Override
+    public String getRamType() {
+        return ramType;
+    }
+
+    @Override
+    public void setRamType(String ramType) {
+        this.ramType = ramType;
+    }
+
+    @Override
+    public int getRamSize() {
+        return ramSize;
+    }
+
+    @Override
+    public void setRamSize(int ramSize) {
+        this.ramSize = ramSize;
+    }
+
+    @Override
+    public String getCpuType() {
+        return cpuType;
+    }
+
+    @Override
+    public void setCpuType(String cpuType) {
+        this.cpuType = cpuType;
+    }
+
+    @Override
+    public int getHddSize() {
+        return hddSize;
+    }
+
+    @Override
+    public void setHddSize(int hddSize) {
+        this.hddSize = hddSize;
+    }
+
+    public int getScreenSize() {
+        return screenSize;
+    }
+
+    public void setScreenSize(int screenSize) {
+        this.screenSize = screenSize;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public Laptop(String name) {
+        this.name = name;
+    }
+}
