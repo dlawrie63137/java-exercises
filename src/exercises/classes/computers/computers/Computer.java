8c3f4dc699ca9086db219dffae2298ef4ea631b9
David Lawrie <dlawrie@att.net>
2019-03-24 09:42:11 -0500

    Cleaned up code

--- src/exercises/classes/computers/computers/Computer.java
+++ src/exercises/classes/computers/computers/Computer.java
@@ -8,43 +8,6 @@ public class Computer {
     protected String cpuType;
     protected int hddSize;
 
-    public String getManufacturer() {
-        return manufacturer;
-    }
 
-    public void setManufacturer(String manufacturer) {
-        this.manufacturer = manufacturer;
-    }
 
-    public String getRamType() {
-        return ramType;
-    }
-
-    public void setRamType(String ramType) {
-        this.ramType = ramType;
-    }
-
-    public int getRamSize() {
-        return ramSize;
-    }
-
-    public void setRamSize(int ramSize) {
-        this.ramSize = ramSize;
-    }
-
-    public String getCpuType() {
-        return cpuType;
-    }
-
-    public void setCpuType(String cpuType) {
-        this.cpuType = cpuType;
-    }
-
-    public int getHddSize() {
-        return hddSize;
-    }
-
-    public void setHddSize(int hddSize) {
-        this.hddSize = hddSize;
-    }
 }
